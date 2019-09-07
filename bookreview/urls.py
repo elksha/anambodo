36f9fc78bd115fd426d4aaf5d7d9544898b6a805
elksha <lksia12@naver.com>
2019-09-08 05:50:54 +0900

    depherokuver0.1

--- bookreview/urls.py
+++ bookreview/urls.py
@@ -19,7 +19,7 @@ from book import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('new/', views.new, name='new'),
     path('detail/<int:post_pk>/', views.detail, name='detail'),
     path('edit/<int:post_pk>/', views.edit, name = 'edit'),
@@ -28,5 +28,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', views.signup, name='signup'),
     path('about/', views.about, name='about'),
-    path('detailto/', views.detail2, name='detail2'),
+    path('', views.detail2, name='detail2'),
 ]
